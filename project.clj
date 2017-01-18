a86cc6d4a0bec8c097bf1975c5ea2db98a6aecea
hokkaido <hello@kimchi.io>
2017-01-18 19:23:08 +0100

    chore(setup): initial project skeleton

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mirage "0.1.0-SNAPSHOT"
+  :description "Mirage"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [org.omcljs/om "1.0.0-alpha47"]
+                 [figwheel-sidecar "0.5.8" :scope "test"]])
